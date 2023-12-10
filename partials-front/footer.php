24eef917f2917ac1148a1ba182f5b8947daf4cb1
Syed Noor <syednoor.n.s311@gmail.com>
2023-12-11 00:30:11 +0600

    WEB_PROJECT_GROUP22

---
+++ partials-front/footer.php
@@ -0,0 +1,29 @@
+<!-- social Section Starts Here -->
+<section class="social">
+        <div class="container text-center">
+            <ul>
+                <li>
+                    <a href="#"><img src="https://img.icons8.com/fluent/50/000000/facebook-new.png"/></a>
+                </li>
+                <li>
+                    <a href="#"><img src="https://img.icons8.com/fluent/48/000000/instagram-new.png"/></a>
+                </li>
+                <li>
+                    <a href="#"><img src="https://img.icons8.com/fluent/48/000000/twitter.png"/></a>
+                </li>
+            </ul>
+        </div>
+    </section>
+    <!-- social Section Ends Here -->
+
+    <!-- footer Section Starts Here -->
+    <section class="footer">
+        <div class="container text-center">
+            <p>Handcrafted by group 22 &copy; 
+            <span id="date">2023</span>. all rights reserved</p>
+        </div>
+    </section>
+    <!-- footer Section Ends Here -->
+
+</body>
+</html>
